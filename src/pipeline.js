dfbec500b8cd7969bc5850a2cefacb94abc311fd
Deepjyoti Barman <deep.barman30@gmail.com>
2022-02-28 10:59:49 +0530

    Add a single file to handle pipeline API calls

---
+++ src/pipeline.js
@@ -0,0 +1,52 @@
+/**
+ * Handle all methods related to hitting the
+ * pipeline API to create/get/update pipelines
+ * based on the passed data.
+ */
+
+var fetch = require("node-fetch")
+
+module.exports = {
+    create: async function (url, body) {
+        /**
+         * Create the pipeline based on the passed
+         * form data. We just need to make a POST
+         * request to the pipeline endpoint to create
+         * the pipeline.
+         * 
+         * The pipeline ID will be picked up from the file
+         * so we need to make sure the pipeline file contains
+         * the proper pipeline ID.
+         * 
+         * @param {string} url - URL to make the request to
+         * @param {FormData} body - FormData body to send in the
+         * request.
+         */
+        const response = await fetch(url, {
+            method: "POST",
+            body: body,
+        })
+    },
+    update: async function (url, body, pipelineID) {
+        /**
+         * Update the pipeline based on the passed
+         * details.
+         * 
+         * This method will not check if the pipeline exists
+         * with the passed ID. That should be checked in the
+         * parent.
+         * 
+         * We need to send a PUT request to the appbaseio
+         * URL with the pipeline ID and the files.
+         * 
+         * @param {string} url - URL of the appbase.io instance
+         * @param {FormData} body - FormData body to send for update.
+         * @param {string} pipelineID - Pipeline ID to update with.
+         */
+        const URL = `${url}/_pipeline/${pipelineID}`
+        const response = await fetch(URL, {
+            method: "PUT",
+            body: body
+        })
+    },
+}
