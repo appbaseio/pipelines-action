de06cdda0337806cc95b8c95042715ef7f4eb2c2
Deepjyoti Barman <deep.barman30@gmail.com>
2022-02-28 10:33:05 +0530

    Add init create file for pipeline

---
+++ src/create.js
@@ -0,0 +1,25 @@
+/**
+ * Handle creating a pipeline based on the passed
+ * details.
+ */
+
+module.exports = {
+    createPipeline: function (url, body) {
+        /**
+         * Create the pipeline based on the passed
+         * form data. We just need to make a POST
+         * request to the pipeline endpoint to create
+         * the pipeline.
+         * 
+         * The pipeline ID will be picked up from the file
+         * so we need to make sure the pipeline file contains
+         * the proper pipeline ID.
+         * 
+         * @param {string} url - URL to make the request to
+         * @param {FormData} body - FormData body to send in the
+         * request.
+         */
+
+
+    }
+}
