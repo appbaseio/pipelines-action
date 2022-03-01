463511fd8669f0b3a2596eefd33341126933593c
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-01 10:32:50 +0530

    Add info logs for index

--- src/index.js
+++ src/index.js
@@ -31,21 +31,27 @@ async function main() {
         const dependencies = core.getInput("depends");
 
         // Clean the pipeline ID to make it usable
+        core.info("Cleaning up the passed pipeline ID to make it URL safe")
         var pipelineID = util.cleanPipelineID(origPipelineID);
 
         // Check what action to do, i:e create or update
         // We can check this by checking whether the pipeline with the
         // passed ID is already present or not.
+        core.info("Trying to get the pipeline from upstream")
         const pipelineFetched = await pipeline.get(appbaseURL, pipelineID)
+        core.info("Pipeline fetch completed")
 
         // Update the action accordingly
         const action = pipelineFetched == null ? 'create' : 'update';
+        core.info(`Action determined based on fetched pipeline: ${action}`)
 
         // Generate form data
         // Thie method will also make sure the `id` field is set to the
         // one passed by user so that the create works properly!
+        core.info("Generating form data to be passed based on passed files...")
         const formData = file.buildFormData(pipelineFile, dependencies, pipelineID)
 
+        core.info(`${action.slice(0, action.length - 1)}ing pipeline using ID: ${pipelineID}`)
         switch (action) {
             case "create":
                 // Create the pipeline
